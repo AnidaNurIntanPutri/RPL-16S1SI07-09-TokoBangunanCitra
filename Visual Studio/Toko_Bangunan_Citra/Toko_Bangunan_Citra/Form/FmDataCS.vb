694099772f708373c686de9d802d07cb70438c8f
pakdetoni <43666717+pakdetoni@users.noreply.github.com>
2018-12-03 18:13:57 +0700

    aplikasi

---
+++ Visual Studio/Toko_Bangunan_Citra/Toko_Bangunan_Citra/Form/FmDataCS.vb
@@ -0,0 +1,35 @@
+﻿Public Class FmDataCS
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+
+
+    Private Sub FormDataCustomerService_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
