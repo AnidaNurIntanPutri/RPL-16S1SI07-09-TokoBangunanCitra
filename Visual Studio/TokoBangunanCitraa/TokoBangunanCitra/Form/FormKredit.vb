5d7b2152b2d3c69fbda56437db29544cdc8a40e2
AnidaNurIntanPutri <anida.putri@students.amikom.ac.id>
2018-12-06 07:40:08 +0700

    visual basic

---
+++ Visual Studio/TokoBangunanCitraa/TokoBangunanCitra/Form/FormKredit.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormKredit
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
