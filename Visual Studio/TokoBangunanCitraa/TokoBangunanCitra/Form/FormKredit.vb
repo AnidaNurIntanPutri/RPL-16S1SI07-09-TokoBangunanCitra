b68da6036e1d892535bfb3e66c39dcfc26da1cc3
wahidnurhadi <43665721+wahidnurhadi@users.noreply.github.com>
2018-12-06 08:06:33 +0700

    aplikasi

--- Visual Studio/TokoBangunanCitraa/TokoBangunanCitra/Form/FormKredit.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class FormKredit
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub Label7_Click(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
